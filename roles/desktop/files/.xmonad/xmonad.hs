32d964b9cb8c3041322a3b914e4675bb69d71c3f
Zbigniew Pieślak <zpieslak@gmail.com>
2022-08-06 20:46:55 +0200

    Update xmonad
    Remove mousebindings
    Move chromium default startup sites to profile

--- roles/desktop/files/.xmonad/xmonad.hs
+++ roles/desktop/files/.xmonad/xmonad.hs
@@ -9,6 +9,7 @@ main = xmonad $ def
     terminal = "alacritty",
     modMask = mod4Mask,
     borderWidth = 0,
+    focusFollowsMouse = False,
     logHook = fadeWindowsLogHook $ composeAll [
       opaque,
       isUnfocused --> transparency 0.3
@@ -18,13 +19,11 @@ main = xmonad $ def
       className =? "Alacritty" --> doShift "1",
       className =? "KeePassXC" --> doShift "2",
       className =? "Chromium" --> doShift "3",
-      className =? "Min" --> doShift "4",
-      className =? "VirtualBox Manager" --> doShift "9",
-      className =? "VirtualBox Machine" --> doShift "9"
+      className =? "Min" --> doShift "4"
     ] <+> manageSpawn <+> manageHook def,
     startupHook = do
       spawnOnce "alacritty"
-      spawnOnce "chromium gmail.com app.slack.com/client teams.microsoft.com live.com"
+      spawnOnce "chromium"
       spawnOnce "min"
       spawnOnce "keepassxc"
   } `additionalKeysP` [
@@ -36,4 +35,8 @@ main = xmonad $ def
     ("<XF86AudioRaiseVolume>", spawn "~/bin/dunst/volume.sh 2dB+ unmute"),
     ("<XF86AudioMute>", spawn "~/bin/dunst/volume.sh mute"),
     ("<XF86Calculator>", spawn "mate-calculator")
+  ] `removeMouseBindings` [
+    (mod4Mask, button1),
+    (mod4Mask, button2),
+    (mod4Mask, button3)
   ]

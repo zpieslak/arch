0cd714f91c4acf01f0b0984c63e19b9775632e55
Zbigniew Pieślak <zpieslak@gmail.com>
2022-08-08 18:58:05 +0200

    Add Lenovo Ideapad 5
    Minor linter fixes
    Minor script fixes

--- roles/home_assistant/files/bin/backup_hass.sh
+++ roles/home_assistant/files/bin/backup_hass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # hass
-tar --exclude='hass/deps' -zcvf /wd2/backup/hass/$(date +"%Y-%m-%d-%H.%M.%S").tar.gz -C /var/lib/private/ hass/
+tar --exclude='hass/deps' --exclude='hass/.cache' -zcvf /wd2/backup/hass/$(date -u +"%Y-%m-%dT%H:%M:%SZ").tar.gz -C /var/lib/private/ hass/
--- roles/node/files/.bash_login.d/20-nodenv.sh
+++ roles/node/files/.bash_login.d/20-nodenv.sh
@@ -3,3 +3,14 @@ export PATH="$HOME/.nodenv/bin:$PATH"
 
 # Initialize rbenv
 eval "$(nodenv init -)"
+
+# Functions
+yarn() {
+  $(pwd)/node_modules/yarn/bin/yarn "$@"
+}
+export -f yarn
+
+prettier() {
+  $(pwd)/node_modules/prettier/bin-prettier.js "$@"
+}
+export -f prettier

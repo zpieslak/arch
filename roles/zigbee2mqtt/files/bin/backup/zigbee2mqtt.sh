a17b476e9aba22f96dc85433a59b7cf2b996e2ed
Zbigniew Pieślak <zpieslak@gmail.com>
2024-04-07 17:19:30 +0200

    Rename pi to core

--- roles/zigbee2mqtt/files/bin/backup/zigbee2mqtt.sh
+++ roles/zigbee2mqtt/files/bin/backup/zigbee2mqtt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # zigbee2mqtt
-tar --exclude={zigbee2mqtt/log,zigbee2mqtt/*.json} -zcvf /media/backup1/pi/zigbee2mqtt/$(date -u +"%Y-%m-%dT%H-%M-%SZ").tar.gz -C /var/lib/ zigbee2mqtt/
+tar --exclude={zigbee2mqtt/log,zigbee2mqtt/*.json} -zcvf /media/backup1/core/zigbee2mqtt/$(date -u +"%Y-%m-%dT%H-%M-%SZ").tar.gz -C /var/lib/ zigbee2mqtt/

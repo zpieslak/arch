fd62571c01823cf51abc1999df7bf81d1b026db9
Daniel Jensen <dhjen@outlook.com>
2025-10-12 11:00:55 +0200

    Adjusted for my needs

--- roles/desktop/files/.bash_login.d/80-xinit.sh
+++
@@ -1,4 +0,0 @@
-# Run X on stratup only on /dev/tty1
-if [[ "$(tty)" = "/dev/tty1" ]] && [[ -z "$DISPLAY" ]] && [[ "$XDG_VTNR" -eq 1 ]]; then
-  exec startx
-fi
--- roles/desktop/files/bin/screenshot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-maim -s -u ~/Downloads/screenshot_$(date -u +"%Y%m%dT%H%M%SZ").png
